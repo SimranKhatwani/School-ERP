7584cf46b1135170841d66d96a3871544aaac91e
Simran Khatwani <simrankhatwani2807@gmail.com>
2026-06-27 16:04:46 +0530

    installed libraries for react

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import { Home } from "lucide-react";
 
 const App = () => {
   return (
     <div>
         <div>
+          <Home color="pink" size={32} strokeWidth={4} />
             <h1 className="text-amber-800">hello simran</h1>
         </div>
     </div>
